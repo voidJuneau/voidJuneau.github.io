f587a736896aac1525961ec96f504b39698f3c7c
Juneau <33215669+voidJuneau@users.noreply.github.com>
2020-09-01 12:17:22 -0400

    fixed line breaking error

--- projects.js
+++ projects.js
@@ -6,25 +6,25 @@ const projects = [
         url: "https://github.com/voidJuneau/grateful",
         img: "https://raw.githubusercontent.com/voidJuneau/grateful/master/screenshot.png",
         title: "Greatful",
-        detail: "A full stack web application with NoSQL DB to help mental problem." +
-         "<br>node.js, Express.js, MongoDB, React.js"
+        detail: "A full stack web application with NoSQL DB to help mental problem. " +
+         "\nnode.js, Express.js, MongoDB, React.js"
     },
     
     {
         url: "https://play.google.com/store/apps/details?id=im.juneau.discretemathformulas",
         img: "https://raw.githubusercontent.com/ljuneaul/DiscreteMathFormulas/master/screenshot.png",
         title: "Discrete Math & Statistics Formulas",
-        detail: "The application provides not only all the formulas but also" +
-         "thoughtfully curated useful videos which will help your understanding." +
-         "<br>Java for android"
+        detail: "The application provides not only all the formulas but also " +
+         "thoughtfully curated useful videos which will help your understanding. " +
+         "\nJava for android"
     },
     
     {
         url: "https://github.com/ljuneaul/kiosk",
         img: "https://raw.githubusercontent.com/ljuneaul/kiosk/master/screenshot.png",
         title: "Tom Kiosk",
-        detail: "Functioning mockup Web page." +
-        "<br>JavaScript, Vue.js, HTML, Bootstrap, Sass"
+        detail: "Functioning mockup Web page. " +
+        "\nJavaScript, Vue.js, HTML, Bootstrap, Sass"
     },
     
     {
